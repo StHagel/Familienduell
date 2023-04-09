c48809ef43791bf0a6ddf08a916db1d19e8a91b7
sthagel <sthagel@uw.edu>
2023-04-09 23:31:44 +0200

    Fix title for submitted.php page

--- submitted.php
+++ submitted.php
@@ -6,7 +6,7 @@
 -->
 <html lang="DE">
     <head>
-        <title>Twenty by HTML5 UP</title>
+        <title>Rebecca ♥ Max</title>
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
         <link rel="stylesheet" href="assets/css/main.css"/>
